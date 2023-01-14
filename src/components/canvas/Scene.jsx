13bc06d70d5b8680715df453d6ad21f6f12a2f71
Thakisis <sub722@gmail.com>
2023-01-14 20:33:41 +0100

    added preload using threee and gsap and math

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -1,22 +1,28 @@
+
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { GridFloor } from './GridFloor'
 import { Camera } from './Camera'
-import { Disposables } from './Disposables'
+import { LightEffects } from './LightEffects'
+import { GetScene } from './GetScene'
+import ComputerNetwork from './Objects3D/ComputerNetwork'
 export default function Scene({ children, ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
+
   return (
     <Canvas {...props}>
 
       <directionalLight intensity={0.75} />
       <ambientLight intensity={0.75} />
       <Camera />
-
+      <LightEffects></LightEffects>
+      <ComputerNetwork></ComputerNetwork>
       <GridFloor />
-      <Disposables></Disposables>
       <Perf overClock />
       <OrbitControls />
+      <GetScene />
+
     </Canvas>
   )
 }
